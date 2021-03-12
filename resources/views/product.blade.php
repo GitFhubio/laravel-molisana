f0ef2d4efd5ea28fd0e2b0c3367cc432bfa8c7b5
Fabio Petrone <fa610movic90@gmail.com>
2021-03-12 18:12:47 +0100

    if dentro blade

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,14 +1,3 @@
-@php
-    if ($idProduct>1){
-    $prev = $idProduct - 1 ;}
-    else {
-    $prev = count($array) - 1;}
-    if ($idProduct< count($array) - 1){
-    $post = $idProduct + 1 ;}
-    else {
-    $post = 1 ;}
-@endphp
-
 @extends('layout.app')
 @section('content')
  {{-- $idProduct --}}
@@ -21,15 +10,27 @@
 {{--non stampo come stringa ma come html con {!!  !!} --}}
  <div class="descrizione"><p>{!! $array[$idProduct - 1]['descrizione'] !!}</p></div>
 </div>
- </div>
-<a href="/product/{{$prev }}"><div class="angle-left">
-<i class="fas fa-angle-left"></i>
+</div>
+@if ($idProduct>1)
+  <a href="/product/{{ $idProduct - 1 }}"><div class="angle-left">
+  <i class="fas fa-angle-left"></i>
+  </div></a>
+@else
+  <a href="/product/{{ count($array) - 1 }}"><div class="angle-left">
+  <i class="fas fa-angle-left"></i>
 </div></a>
-<a href="/product/{{$post }}"><div class="angle-right">
+@endif
+@if ($idProduct< count($array) - 1)
+<a href="/product/{{$idProduct + 1 }}"><div class="angle-right">
 <i class="fas fa-angle-right"></i>
 </div></a>
-  </div>
+@else
+  <a href="/product/{{1}}"><div class="angle-right">
+  <i class="fas fa-angle-right"></i>
+  </div></a>
+@endif
 
+  </div>
 @endsection
 
 {{-- oppure direttamente in html --}}
